bab97135784faedcdc74c603826f2b447b2c9900
Michael <mikahado@gmail.com>
2022-10-23 23:02:51 -0400

    props code along

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 function BlogContent(props) {
-  return <div id="blog-content">{props.articleText}</div>;
+  console.log(props);
+
+  if(!props.isPublished){
+    return null
+  } else {
+    return (
+    <div>
+      <h1>{props.articleText}</h1>
+      <p>{props.minutesToRead} minutes to read the thing ok?</p>
+    </div>
+  )}
+
+  // return <div id="blog-content">{props.articleText}</div>;
 }
 
 export default BlogContent;
